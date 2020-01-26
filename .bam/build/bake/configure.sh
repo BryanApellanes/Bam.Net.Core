566a9443a577e573c438548fd0f197e0569ff673
Bryan Apellanes <bryan@threeheadz.com>
2020-01-26 09:10:29 -0800

    updated versions; wip version handling build scripts

--- .bam/build/bake/configure.sh
+++ .bam/build/bake/configure.sh
@@ -4,4 +4,7 @@ printf "Configuring build...\r\n"
 
 source ../common/init.sh
 
-#./set-version.sh
+./discover-tools-recipe.sh
+./discover-lib-recipe.sh
+
+./write-version-code.sh
--- .bam/build/bake/set-version.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source ./init.sh
-
-./discover-tools-recipe.sh
-./discover-lib-recipe.sh
-
-./write-version-code.sh
