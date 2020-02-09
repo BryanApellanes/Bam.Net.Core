c2a3217e492496234c217412e8840c044c678c05
Bryan Apellanes <bryan@threeheadz.com>
2020-02-09 12:40:51 -0800

    ActionExecutor requires apikey

--- _lib/bam.net.presentation/ActionProviders/ActionExecutor.cs
+++ _lib/bam.net.presentation/ActionProviders/ActionExecutor.cs
@@ -1,11 +1,13 @@
 using System;
 using Bam.Net.Logging;
 using Bam.Net.ServiceProxy;
+using Bam.Net.ServiceProxy.Secure;
 
 namespace Bam.Net.Presentation.ActionProviders
 {
     [Serializable]
     [Proxy("actionExecutor")]
+    [ApiKeyRequired]
     public class ActionExecutor : Loggable
     {
         public ActionExecutor(ILogger logger = null)
