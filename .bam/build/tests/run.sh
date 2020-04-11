754e77f7c354b239dc34ecdc935c0fad25623474
Bryan Apellanes <bryan@threeheadz.com>
2020-04-11 11:56:37 -0700

    updated build scripts

--- .bam/build/tests/run.sh
+++ .bam/build/tests/run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# specify /Group:<testGroupName> to run tests for a specific test group
+
 source ../common/init.sh
 
 ensure_bamtest
 
-${BAMTEST} /Recipe:./recipes/${RUNTIME}-bamtoolkit-tests.json
+printf "executing=>${BAMTEST} /Recipe:`pwd`/recipes/${RUNTIME}-bamtoolkit-tests.json $1 $2 $3"
+${BAMTEST} /Recipe:`pwd`/recipes/${RUNTIME}-bamtoolkit-tests.json $1 $2 $3
--- _tests/core/bam.net.incubation.core.tests/SemanticAssemblyInfo.cs
+++ _tests/core/bam.net.incubation.core.tests/SemanticAssemblyInfo.cs
@@ -2,6 +2,6 @@ using System.Reflection;
 using Bam.Net;
 
 [assembly: AssemblyVersion("1.0.5.0")]
-[assembly: AssemblyFileVersion("1.0.5.2769504898")]
-[assembly: AssemblyCommit("9d3bcc0")]
-[assembly: AssemblySemanticVersion("1.0.5-test+9d3bcc0")]
+[assembly: AssemblyFileVersion("1.0.5.509944545")]
+[assembly: AssemblyCommit("7787e60")]
+[assembly: AssemblySemanticVersion("1.0.5-test+7787e60")]
