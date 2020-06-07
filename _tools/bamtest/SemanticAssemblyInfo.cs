62b576809d8a90507049e10682aefd5bbcdde133
Bryan Apellanes <bryan@threeheadz.com>
2020-06-07 11:35:58 -0700

    build scripts and workflow updates

--- _tools/bamtest/SemanticAssemblyInfo.cs
+++ _tools/bamtest/SemanticAssemblyInfo.cs
@@ -2,6 +2,6 @@ using System.Reflection;
 using Bam.Net;
 
 [assembly: AssemblyVersion("1.0.5.0")]
-[assembly: AssemblyFileVersion("1.0.5.356070287")]
-[assembly: AssemblyCommit("5edbb1f")]
-[assembly: AssemblySemanticVersion("1.0.5-test+5edbb1f")]
+[assembly: AssemblyFileVersion("1.0.5.1870011138")]
+[assembly: AssemblyCommit("04d5eb8")]
+[assembly: AssemblySemanticVersion("1.0.5-test+04d5eb8")]
