d171c277b3dc1251db0ee1e0589a2d2ce4be21b0
Bryan Apellanes <bryan@threeheadz.com>
2020-08-02 10:16:24 -0700

    wip: etc credential management

--- _tests/core/bam.net.data.repositories.core.tests/SemanticAssemblyInfo.cs
+++ _tests/core/bam.net.data.repositories.core.tests/SemanticAssemblyInfo.cs
@@ -2,6 +2,6 @@ using System.Reflection;
 using Bam.Net;
 
 [assembly: AssemblyVersion("1.0.5.0")]
-[assembly: AssemblyFileVersion("1.0.5.1870011138")]
-[assembly: AssemblyCommit("04d5eb8")]
-[assembly: AssemblySemanticVersion("1.0.5-test+04d5eb8")]
+[assembly: AssemblyFileVersion("1.0.5.742579000")]
+[assembly: AssemblyCommit("60914af")]
+[assembly: AssemblySemanticVersion("1.0.5-test+60914af")]
