9f896aabe0e0c0554e0a4e068fe8a8b6824cfdcd
Bryan Apellanes <bryan.apellanes@gmail.com>
2020-11-25 07:04:13 -0800

    move bam.net.data.dynamic so it can be made submodule

--- _lib/bam.net.data.dynamic/SemanticAssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Reflection;
-using Bam.Net;
-
-[assembly: AssemblyVersion("1.0.5.0")]
-[assembly: AssemblyFileVersion("1.0.5.1235072338")]
-[assembly: AssemblyCommit("3ca52cf")]
-[assembly: AssemblySemanticVersion("1.0.5-test+3ca52cf")]
