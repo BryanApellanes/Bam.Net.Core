2ff7de60965b0f3b8fb81df819810f21d063a756
Bryan Apellanes <bryan.apellanes@gmail.com>
2020-11-25 09:00:00 -0800

    net5

--- _lib/bam.net.core/_core/_Messaging/HandlebarsEmailComposer.cs
+++ _lib/bam.net.core/_core/_Messaging/HandlebarsEmailComposer.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
-using HandlebarsDotNet;
 using Bam.Net.Presentation.Html;
 using Bam.Net.Presentation.Handlebars;
 
