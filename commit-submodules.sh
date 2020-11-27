b3b2ee92e4c66252498be58876b93a7a96f2fdb7
Bryan Apellanes <bryan@threeheadz.com>
2020-11-27 10:27:27 -0800

    pre submodule commit: {update}

--- commit-submodules.sh
+++ commit-submodules.sh
@@ -9,6 +9,8 @@ if [[ $1 = "-help" ]] || [[ $1 = "-?" ]] || [[ $1 = "-h" ]]; then
     exit 0;
 fi
 
+
+
 BOLD=$(tput bold)
 BLACK=$(tput setaf 0)
 WHITE=$(tput setaf 7)
@@ -16,17 +18,21 @@ BLUE=$(tput setaf 4)
 GREEN=$(tput setaf 2)
 NORMAL=$(tput sgr0)
 
-# Figure out if the parent repo has changed and make a commit with the
-# commit message that was passed in
-STATUS=$(git status --porcelain --ignore-submodules | wc -l)
-if [[ $STATUS -ne 0 ]]; then
-  echo "🤓  ${GREEN}UPDATING PARENT REPO${NORMAL}"
-  git add .
-  git commit -am "$1"
-  if [[ $? -ne 0 ]]; then
-    exit 1;
+function commitParent(){
+  # Figure out if the parent repo has changed and make a commit with the
+  # commit message that was passed in
+  STATUS=$(git status --porcelain --ignore-submodules | wc -l)
+  if [[ $STATUS -ne 0 ]]; then
+    echo "🤓  ${GREEN}UPDATING PARENT REPO${NORMAL}"
+    git add .
+    git commit -am "${1}"
+    if [[ $? -ne 0 ]]; then
+      exit 1;
+    fi
   fi
-fi
+}
+
+commitParent "pre submodule commit: {$1}"
 
 # Now go to each submodule and update those with the same commit message
 SUBMODULES=($(git submodule | awk '{print $2}'))
@@ -56,6 +62,9 @@ if [[ $SUBMODULES_UPDATED -eq 1 ]]; then
   git add .
   git commit -am "Updated submodules: ${1}"
 fi
+
+# commit parent again
+commitParent "post submodule commit: {$1}"
 
 # Push everything
 echo "🤓  ${GREEN}UPDATING EVERYTHING${NORMAL}"
