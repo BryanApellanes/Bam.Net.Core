96050c43f13db26787eb0984d34a6f04e7e91756
Bryan Apellanes <bryan.apellanes@gmail.com>
2020-12-13 10:23:19 -0800

    ${1}

--- _tools/masuco/SemanticAssemblyInfo.cs
+++ _tools/masuco/SemanticAssemblyInfo.cs
@@ -2,6 +2,6 @@ using System.Reflection;
 using Bam.Net;
 
 [assembly: AssemblyVersion("1.0.5.0")]
-[assembly: AssemblyFileVersion("1.0.5.1367460201")]
-[assembly: AssemblyCommit("0c1ef65")]
-[assembly: AssemblySemanticVersion("1.0.5-test+0c1ef65")]
+[assembly: AssemblyFileVersion("1.0.5.153843750")]
+[assembly: AssemblyCommit("34ddaf1e")]
+[assembly: AssemblySemanticVersion("1.0.5-test+34ddaf1e")]
