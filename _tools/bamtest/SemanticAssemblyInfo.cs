da7c415a58e8bdaf27cc571336390807046ffacc
Bryan Apellanes <bryan.apellanes@gmail.com>
2020-12-22 09:51:37 -0800

    ${1}

--- _tools/bamtest/SemanticAssemblyInfo.cs
+++ _tools/bamtest/SemanticAssemblyInfo.cs
@@ -2,6 +2,8 @@ using System.Reflection;
 using Bam.Net;
 
 [assembly: AssemblyVersion("1.0.5.0")]
-[assembly: AssemblyFileVersion("1.0.5.300693035")]
-[assembly: AssemblyCommit("04b91cb1")]
-[assembly: AssemblySemanticVersion("1.0.5-test+04b91cb1")]
+[assembly: AssemblyFileVersion("1.0.5.0")]
+[assembly: AssemblyCommit("5d2c576e")]
+[assembly: AssemblySemanticVersion("1.0.5-test+5d2c576e")]
+[assembly: AssemblyDescription("; SemanticVersion=1.0.5-test+5d2c576e, Revision=1.0.5.1535760797")]
+[assembly: AssemblyProduct("bamtest")]
--- commit-submodules.sh
+++ commit-submodules.sh
@@ -17,20 +17,21 @@ GREEN=$(tput setaf 2)
 NORMAL=$(tput sgr0)
 
 function commitParent(){
+  MSG=$1
   # Figure out if the parent repo has changed and make a commit with the
   # commit message that was passed in
   STATUS=$(git status --porcelain --ignore-submodules | wc -l)
   if [[ $STATUS -ne 0 ]]; then
     echo "🤓  ${GREEN}UPDATING PARENT REPO${NORMAL}"
     git add .
-    git commit -am '${1}'
+    git commit -am "${MSG}"
     if [[ $? -ne 0 ]]; then
       exit 1;
     fi
   fi
 }
 
-commitParent "pre submodule commit: {$1}"
+commitParent "pre submodule commit: ${1}"
 
 # Now go to each submodule and update those with the same commit message
 SUBMODULES=($(git submodule | awk '{print $2}'))
@@ -39,12 +40,12 @@ for SUBMODULE in "${SUBMODULES[@]}"; do
   pushd $SUBMODULE > /dev/null
   STATUS=$(git status --porcelain | wc -l)
   if [[ -f "./commit-submodules.sh" ]]; then
-    ./commit-submodules.sh $1
+    ./commit-submodules.sh ${1}
   fi
   if [[ $STATUS -ne 0 ]]; then
     echo "🤓  ${GREEN}UPDATING SUBMODULE:${WHITE} ${SUBMODULE}${NORMAL}"
     git add .
-    git commit -am '$1'
+    git commit -am "${1}"
     if [[ $? -ne 0 ]]; then
       exit 1;
     fi
@@ -62,7 +63,7 @@ if [[ $SUBMODULES_UPDATED -eq 1 ]]; then
 fi
 
 # commit parent again
-commitParent "post submodule commit: {$1}"
+commitParent "post submodule commit: ${1}"
 
 # Push everything
 echo "🤓  ${GREEN}UPDATING EVERYTHING${NORMAL}"
