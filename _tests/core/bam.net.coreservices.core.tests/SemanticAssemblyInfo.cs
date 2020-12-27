73bc6f49f090f872adb83a183ef77295e0adc3c5
Bryan Apellanes <bryan.apellanes@gmail.com>
2020-12-27 07:03:27 -0800

    pre submodule commit: update build scripts

--- _tests/core/bam.net.coreservices.core.tests/SemanticAssemblyInfo.cs
+++ _tests/core/bam.net.coreservices.core.tests/SemanticAssemblyInfo.cs
@@ -3,7 +3,7 @@ using Bam.Net;
 
 [assembly: AssemblyVersion("1.0.5.0")]
 [assembly: AssemblyFileVersion("1.0.5.0")]
-[assembly: AssemblyCommit("52e96770")]
-[assembly: AssemblySemanticVersion("1.0.5-test+52e96770")]
-[assembly: AssemblyDescription("; SemanticVersion=1.0.5-test+52e96770, Revision=1.0.5.47245066")]
+[assembly: AssemblyCommit("d554f570")]
+[assembly: AssemblySemanticVersion("1.0.5-test+d554f570")]
+[assembly: AssemblyDescription("SemanticVersion=1.0.5-test+d554f570, Revision=1.0.5.235915801")]
 [assembly: AssemblyProduct("bam.net.coreservices.core.tests")]
