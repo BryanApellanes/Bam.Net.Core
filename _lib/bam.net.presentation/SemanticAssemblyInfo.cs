f7ec10ea9b2e619ed0e4d66a876c4275845ef951
Bryan Apellanes <bryan.apellanes@gmail.com>
2020-12-28 18:23:40 -0800

    delete _lib/bam.net.presentation; it moved to submodule of BamAppServices

--- _lib/bam.net.presentation/SemanticAssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-using System.Reflection;
-using Bam.Net;
-
-[assembly: AssemblyVersion("1.0.5.0")]
-[assembly: AssemblyFileVersion("1.0.5.0")]
-[assembly: AssemblyCommit("46698bb7")]
-[assembly: AssemblySemanticVersion("1.0.5-test+46698bb7")]
-[assembly: AssemblyDescription("SemanticVersion=1.0.5-test+46698bb7, Revision=1.0.5.921560188")]
-[assembly: AssemblyProduct("bam.net.presentation")]
