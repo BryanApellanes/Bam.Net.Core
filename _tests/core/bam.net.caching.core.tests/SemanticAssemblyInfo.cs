4e99008ed81df79e1749688d2fb22026bf08e18a
Bryan Apellanes <bryan@threeheadz.com>
2020-12-30 09:01:09 -0800

    pre submodule commit: submodules

--- _tests/core/bam.net.caching.core.tests/SemanticAssemblyInfo.cs
+++ _tests/core/bam.net.caching.core.tests/SemanticAssemblyInfo.cs
@@ -3,7 +3,7 @@ using Bam.Net;
 
 [assembly: AssemblyVersion("1.0.5.0")]
 [assembly: AssemblyFileVersion("1.0.5.0")]
-[assembly: AssemblyCommit("458e45e0")]
-[assembly: AssemblySemanticVersion("1.0.5-test+458e45e0")]
-[assembly: AssemblyDescription("SemanticVersion=1.0.5-test+458e45e0, Revision=1.0.5.1017980963")]
+[assembly: AssemblyCommit("cefacc4")]
+[assembly: AssemblySemanticVersion("1.0.5-test+cefacc4")]
+[assembly: AssemblyDescription("SemanticVersion=1.0.5-test+cefacc4, Revision=1.0.5.4073017496")]
 [assembly: AssemblyProduct("bam.net.caching.core.tests")]
