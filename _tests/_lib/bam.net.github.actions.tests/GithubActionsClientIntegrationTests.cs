32ad611b20d1c24eff2f3dc1694b425f79cba2e7
Bryan Apellanes <bryan.apellanes@gmail.com>
2021-01-01 11:25:12 -0800

    update tests

---
+++ _tests/_lib/bam.net.github.actions.tests/GithubActionsClientIntegrationTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using Bam.Net.CommandLine;
+using bam.net.github.actions;
+using Bam.Net.Testing.Integration;
+
+namespace Bam.Net.Github.Actions.Tests
+{
+    [Serializable]
+    public class IntegrationTests
+    {
+        [ConsoleAction]
+        [IntegrationTest]
+        public void CanGetArtifacts()
+        {
+            GithubActionsClient client = new GithubActionsClient();
+            GithubArtifact[] artifacts = client.GetArtifacts("BryanApellanes", "Bam.Net.Core").ToArray();
+            artifacts.ShouldNotBeNull("artifacts was null");
+            artifacts.Length.ShouldBeGreaterThan(0, "No artifacts were returned");
+            artifacts.Each(artifact =>
+            {
+                Message.PrintLine(artifact.ToJson(true), ConsoleColor.Cyan);
+            });
+        }
+    }
+}
