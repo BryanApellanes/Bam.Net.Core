3629825bc6042993164907101597ee643e944a3a
Bryan Apellanes <bryan.apellanes@gmail.com>
2021-01-01 11:25:36 -0800

    pre submodule commit: update github actions artifact handling

--- _tests/_lib/bam.net.github.actions.tests/GithubActionsClientIntegrationTests.cs
+++ _tests/_lib/bam.net.github.actions.tests/GithubActionsClientIntegrationTests.cs
@@ -1,7 +1,8 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 using Bam.Net.CommandLine;
-using bam.net.github.actions;
+using Bam.Net.Github.Actions;
 using Bam.Net.Testing.Integration;
 
 namespace Bam.Net.Github.Actions.Tests
@@ -21,6 +22,22 @@ namespace Bam.Net.Github.Actions.Tests
             {
                 Message.PrintLine(artifact.ToJson(true), ConsoleColor.Cyan);
             });
+        }
+
+        [ConsoleAction]
+        [IntegrationTest]
+        public void CanDownloadArtifact()
+        {
+            GithubActionsClient client = new GithubActionsClient();
+            GithubArtifact[] artifacts = client.GetArtifacts("BryanApellanes", "Bam.Net.Core").ToArray();
+            artifacts.Length.ShouldBeGreaterThan(0, "No artifacts were returned");
+            GithubArtifact artifact = artifacts.First();
+            string fileName = $"./{nameof(CanDownloadArtifact)}.zip";
+            FileInfo fileInfo = artifact.DownloadTo(fileName);
+            fileInfo.Exists.ShouldBeTrue("file doesn't exists after attempted download");
+            
+            fileInfo.Length.ShouldBeGreaterThanOrEqualTo(artifact.SizeInBytes);
+            Message.PrintLine("Artifact Size: {0}, File Size: {1}", artifact.SizeInBytes, fileInfo.Length);
         }
     }
 }
