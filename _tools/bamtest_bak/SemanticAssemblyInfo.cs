22c05fb737dce26f1d1c228676c1c20a0047123a
Bryan Apellanes bryan.apellanes@okta.com <bryan.apellanes@okta.com>
2021-01-30 07:25:38 -0800

    move bamtest to prepare to make it a submodule

---
+++ _tools/bamtest_bak/SemanticAssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+using Bam.Net;
+
+[assembly: AssemblyVersion("1.0.5.0")]
+[assembly: AssemblyFileVersion("1.0.5.0")]
+[assembly: AssemblyCommit("e189dfb6")]
+[assembly: AssemblySemanticVersion("1.0.5-test+e189dfb6")]
+[assembly: AssemblyDescription("SemanticVersion=1.0.5-test+e189dfb6, Revision=1.0.5.4278396549")]
+[assembly: AssemblyProduct("bamtest")]
