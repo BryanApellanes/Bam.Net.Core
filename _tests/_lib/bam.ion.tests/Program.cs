9520dc6fb94792fe338c6227a908ae89c1222750
Bryan Apellanes <bryan@threeheadz.com>
2021-04-11 15:29:05 -0700

    pre submodule commit: [wip] ion types

---
+++ _tests/_lib/bam.ion.tests/Program.cs
@@ -0,0 +1,13 @@
+﻿using Bam.Net;
+using System;
+
+namespace Bam.Ion.Tests
+{
+    class Program : CommandLineTool
+    {
+        static void Main(string[] args)
+        {
+            ExecuteMainOrInteractive(args);
+        }
+    }
+}
---
+++ _tests/_lib/bam.ion.tests/UnitTests/IonValueTests.cs
@@ -0,0 +1,30 @@
+﻿using Bam.Net;
+using Bam.Net.Testing.Unit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bam.Ion.Tests.UnitTests
+{
+    [Serializable]
+    public class IonValueTests : CommandLineTool
+    {
+        [UnitTest]
+        public async Task IonValueShouldSerializeAsExpected()
+        {
+            string json =
+@"{
+  ""firstName"": ""Bob"",
+  ""lastName"": ""Smith"",
+  ""birthDate"": ""1980-01-23""
+}";
+
+            IonCollection value = IonValue.Read(json);
+            Expect.IsTrue(value.Value is List<IonMember>);
+            string output = value.ToJson(true);
+            Expect.AreEqual(json, output);
+        }
+    }
+}
