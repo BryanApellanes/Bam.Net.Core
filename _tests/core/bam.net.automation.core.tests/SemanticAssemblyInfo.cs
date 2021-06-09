a9e4d68c31b8a4b6a464a045a7fd226573a3e0ab
Bryan Apellanes bryan.apellanes@okta.com <bryan.apellanes@okta.com>
2021-06-09 09:24:41 -0700

    pre submodule commit: wip: ran build

--- _tests/core/bam.net.automation.core.tests/SemanticAssemblyInfo.cs
+++ _tests/core/bam.net.automation.core.tests/SemanticAssemblyInfo.cs
@@ -3,7 +3,7 @@ using Bam.Net;
 
 [assembly: AssemblyVersion("1.0.5.0")]
 [assembly: AssemblyFileVersion("1.0.5.0")]
-[assembly: AssemblyCommit("e189dfb6")]
-[assembly: AssemblySemanticVersion("1.0.5-test+e189dfb6")]
-[assembly: AssemblyDescription("SemanticVersion=1.0.5-test+e189dfb6, Revision=1.0.5.4278396549")]
+[assembly: AssemblyCommit("50f241d3")]
+[assembly: AssemblySemanticVersion("1.0.5-test+50f241d3")]
+[assembly: AssemblyDescription("SemanticVersion=1.0.5-test+50f241d3, Revision=1.0.5.2124251692")]
 [assembly: AssemblyProduct("bam.net.automation.core.tests")]
--- _tools/laotze/SemanticAssemblyInfo.cs
+++ _tools/laotze/SemanticAssemblyInfo.cs
@@ -3,7 +3,7 @@ using Bam.Net;
 
 [assembly: AssemblyVersion("1.0.5.0")]
 [assembly: AssemblyFileVersion("1.0.5.0")]
-[assembly: AssemblyCommit("2599a1d8")]
-[assembly: AssemblySemanticVersion("1.0.5-test+2599a1d8")]
-[assembly: AssemblyDescription("SemanticVersion=1.0.5-test+2599a1d8, Revision=1.0.5.3750768831")]
+[assembly: AssemblyCommit("50f241d3")]
+[assembly: AssemblySemanticVersion("1.0.5-test+50f241d3")]
+[assembly: AssemblyDescription("SemanticVersion=1.0.5-test+50f241d3, Revision=1.0.5.2124251692")]
 [assembly: AssemblyProduct("laotze")]
