55db7d650c5358c8c33bd7f6e97682f79cf67d0b
Bryan Apellanes bryan.apellanes@okta.com <bryan.apellanes@okta.com>
2021-06-11 10:23:19 -0700

    wip: package handling

--- _tests/core/bam.net.incubation.core.tests/SemanticAssemblyInfo.cs
+++ _tests/core/bam.net.incubation.core.tests/SemanticAssemblyInfo.cs
@@ -3,7 +3,7 @@ using Bam.Net;
 
 [assembly: AssemblyVersion("1.0.6.0")]
 [assembly: AssemblyFileVersion("1.0.6.0")]
-[assembly: AssemblyCommit("a39d4372")]
-[assembly: AssemblySemanticVersion("1.0.6-test+a39d4372")]
-[assembly: AssemblyDescription("SemanticVersion=1.0.6-test+a39d4372, Revision=1.0.6.215563036")]
+[assembly: AssemblyCommit("88346f4c")]
+[assembly: AssemblySemanticVersion("1.0.6-88346f4c")]
+[assembly: AssemblyDescription("SemanticVersion=1.0.6-88346f4c, Revision=1.0.6.2662376893")]
 [assembly: AssemblyProduct("bam.net.incubation.core.tests")]
