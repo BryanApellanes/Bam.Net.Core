5cb2911091c16ca4401e95c8ce220eb7d423ef4f
Bryan Apellanes bryan.apellanes@okta.com <bryan.apellanes@okta.com>
2021-06-12 10:47:32 -0700

    pre submodule commit: update build scripts

--- _lib/adhoc/SemanticAssemblyInfo.cs
+++ _lib/adhoc/SemanticAssemblyInfo.cs
@@ -3,7 +3,7 @@ using Bam.Net;
 
 [assembly: AssemblyVersion("1.0.6.0")]
 [assembly: AssemblyFileVersion("1.0.6.0")]
-[assembly: AssemblyCommit("2ac4ccaa")]
-[assembly: AssemblySemanticVersion("1.0.6-2ac4ccaa")]
-[assembly: AssemblyDescription("SemanticVersion=1.0.6-2ac4ccaa, Revision=1.0.6.3823125167")]
+[assembly: AssemblyCommit("eb438701")]
+[assembly: AssemblySemanticVersion("1.0.6-dev+eb438701")]
+[assembly: AssemblyDescription("SemanticVersion=1.0.6-dev+eb438701, Revision=1.0.6.2215280002")]
 [assembly: AssemblyProduct("adhoc")]
